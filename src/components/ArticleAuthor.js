c8f33c752d01c7bbeb15d89293c78548f72f3a65
MarinaYez <mariosadchuk@gmail.com>
2023-03-16 14:01:59 +0200

    commit

---
+++ src/components/ArticleAuthor.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// function ArticleAuthor() {
+//     return (
+//     <div className="article__title__author">
+//             <h4>Author: Mike</h4>
+//             <h4> Published: 06.12.2022</h4>
+//             <h4>Theme: Video cards</h4>
+//     </div> 
+//     );
+//   }
+//   export default ArticleAuthor;
